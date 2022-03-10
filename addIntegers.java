69e9a93048aca988a9abdf1e6ed759ebc750c6f1
Shahd Mustafa <Shahdmu@umich.edu>
2022-03-10 15:13:13 -0500

    add 6 and 7

--- addIntegers.java
+++ addIntegers.java
@@ -4,7 +4,7 @@ public class addIntegers {
     public static void main(String[] args) {
 
         int first = 6;
-        int second = 5;
+        int second = 7;
 
         System.out.println("Enter two numbers: " + first + " " + second);
         int sum = first + second;
