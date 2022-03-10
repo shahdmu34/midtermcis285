5090af2308353dcb0efeab81e78fee3d803f8470
Shahd Mustafa <Shahdmu@umich.edu>
2022-03-10 15:07:10 -0500

    git add int

--- addIntegers.java
+++ addIntegers.java
@@ -1,8 +1,14 @@
-public class integers {
-public static void main(string[] args){
-  for(int i=1; i <= 10; i++)
-   i = i+1;
-   system.out.println("five"+i);
-}
 
+public class AddIntegers {
+
+    public static void main(String[] args) {
+
+        int first = 2;
+        int second = 3;
+
+        System.out.println("Enter two numbers: " + first + " " + second);
+        int sum = first + second;
+
+        System.out.println("The sum is: " + sum);
+    }
 }
